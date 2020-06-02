6e8859bab5f76f30237e67963bbc7fc2184e14a2
nidhishkrishnan <nidhishkrishnan@gmail.com>
2020-06-02 10:21:12 +0200

    Adding Junit 5 dependencies and test case for get authors

--- src/test/java/org/echocat/kata/java/part1/MainAppUnitTest.java
+++ src/test/java/org/echocat/kata/java/part1/MainAppUnitTest.java
@@ -1,9 +1,9 @@
 package org.echocat.kata.java.part1;
 
-import org.junit.Test;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+//import org.junit.Test;
+//
+//import static org.hamcrest.CoreMatchers.is;
+//import static org.junit.Assert.assertThat;
 
 public class MainAppUnitTest {
 
---
+++ src/test/java/org/echocat/kata/java/part1/controller/AuthorControllerTest.java
@@ -0,0 +1,64 @@
+package org.echocat.kata.java.part1.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Collections;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@DisplayName("Testing Author Controller")
+public class AuthorControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private AuthorService authorService;
+
+    @InjectMocks
+    private AuthorController authorController;
+
+
+    @BeforeEach
+    void setMockOutput()
+    {
+        mockMvc = MockMvcBuilders.standaloneSetup(authorController).build();
+    }
+
+    @Test
+    @DisplayName("Test for getting Author details ('v1/authors')")
+    public void getAuthorsTest() throws Exception
+    {
+        when(authorService.getAuthors()).thenReturn(Optional.of(buildAuthors()));
+        mockMvc.perform(MockMvcRequestBuilders.get("/v1/authors")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].firstName", is("someFirstName")))
+                .andExpect(jsonPath("$[0].lastName", is("someLastName")));
+    }
+
+    private List<Author> buildAuthors() {
+        Author author = new Author();
+        author.setEmail("author@email.com");
+        author.setFirstName("someFirstName");
+        author.setLastName("someLastName");
+        return Collections.singletonList(author);
+    }
+
+}
